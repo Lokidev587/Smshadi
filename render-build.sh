0ad059c953a9b41e042bd1bca7e00eb39f45922a
Lokidev587 <lokirascal5@gmail.com>
2025-09-04 02:00:50 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-# Install Chrome and Chromedriver for Render
-apt-get update
-apt-get install -y chromium-browser chromium-chromedriver
-
-# Install Python dependencies
-pip install -r requirements.txt
